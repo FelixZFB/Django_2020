3ad5262145b101c009fc5d673443862a265041e6
Felix Zhang <18200116656@qq.com>
2020-05-08 23:00:51 +0800

    Daily commit

--- 03_test4/test4/booktest/admin.py
+++ 03_test4/test4/booktest/admin.py
@@ -2,7 +2,7 @@
 
 # test4文件夹右键标记为source root
 from django.contrib import admin
-from booktest.models import BookInfo,HeroInfo
+from booktest.models import BookInfo,HeroInfo,AreaInfo
 
 
 # 自定义管理模型类
@@ -15,7 +15,52 @@ class HeroInfoAdmin(admin.ModelAdmin):
     list_display = ['id', 'hname','hgender','hcomment', 'hbook', 'isDelete']
 
 
+
+class AreaStackedInline(admin.StackedInline):
+    '''栏目方式嵌入数据'''
+    # 先写多类的名字
+    model = AreaInfo
+    # 显示空白栏的数量
+    extra = 2
+
+class AreaTabularInline(admin.TabularInline):
+    '''表格方式嵌入数据'''
+    # 先写多类的名字
+    model = AreaInfo
+    # 显示空白栏的数量
+    extra = 2
+
+
+# 地区类
+class AreaInfoAdmin(admin.ModelAdmin):
+    # 数据表显示的内容，即列的标题, title和parent是模型类里面定义的方法
+    list_display = ['id', 'atitle', 'title','aParent', 'parent']
+    list_per_page = 5 # 指定每页显示的数据条数
+    # 显示操作框在底部，默认是顶部
+    actions_on_bottom = True
+    actions_on_top = False
+    # 右侧显示过滤栏,指定过滤字段
+    list_filter = ['atitle']
+    # 显示搜索框,指定搜索字段
+    search_fields = ['atitle']
+
+    # 显示地区元素详情页内容，显示顺序调整
+    # fields = ['aParent', 'atitle']
+    # fieldsets设置更加详细的信息，fields和fieldsets只能使用一个
+    fieldsets = (
+        ('地级市', {'fields': ['atitle']}),
+        ('省份', {'fields': ['aParent']})
+    )
+
+    # 上面查看市时候已经显示了省份信息，我们想在页面显示成都市下面所有区信息
+    # 页面嵌入数据，栏目和表格两种方式
+    # inlines = [AreaStackedInline]
+    inlines = [AreaTabularInline]
+
+
+
 # 注册管理模型类到后台管理中，注意，先自定义后然后再注册，并且注册时候同一个类写在一起注册
 # 注册图书类和自定义的图书模型管理类到后台管理中，注册后刷新后台已经显示所有信息了
 admin.site.register(BookInfo, BookInfoAdmin)
 admin.site.register(HeroInfo, HeroInfoAdmin)
+admin.site.register(AreaInfo, AreaInfoAdmin)
--- 03_test4/test4/booktest/models.py
+++ 03_test4/test4/booktest/models.py
@@ -61,3 +61,28 @@ class HeroInfo(models.Model):
     hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE) # 英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
 
 
+# 定义地区类
+class AreaInfo(models.Model):
+    atitle = models.CharField(max_length=20) # 地区名称
+    aParent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE) # 自关联属性
+
+    # 该魔法方法用于django后台管理, 对象以什么显示，
+    # 此处对象设置为以名称显示,admin.py中如果设置对象展示方式list_display，该方法自动忽略
+    def __str__(self):
+        return self.atitle
+
+    # 自定义title方法，admin.py里面的list_display可以写上面的属性，也可以写方法名称
+    def title(self):
+        return self.atitle
+    # 后台管理可以通过属性点击排序，方法要支持排序，增加下面的排序依据字段
+    title.admin_order_field = 'atitle'
+    # 指定该方法的在后台中的标题名称，相当于重新命名
+    title.short_description = '地区名称'
+
+    # 自定义parent方法
+    def parent(self):
+        if self.aParent is None:
+            return ''
+        else:
+            return self.aParent.atitle
+    parent.short_description = '父级地区名称'
